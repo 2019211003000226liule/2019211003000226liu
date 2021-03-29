d5c4bcb8699371027d9c92f9cb7cca2a67fd8faf
2019211003000226liule <1478108793@qq.com>
2021-03-29 23:59:50 +0800

    HW-week3 commit

---
+++ src/main/java/com/liule/week3/demo/RegisterServlet.java
@@ -0,0 +1,32 @@
+package com.liule.week3.demo;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+public class RegisterServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String s;
+        String username = request.getParameter( s:"username");
+        String password = request.getParameter( s: "password");
+        String email = request.getParameter( s: "email");
+        String gender = request.getParameter( s: "gender");
+        String birthDate = request.getParameter( s: "birthDate");
+
+        PrintWriter writer = response.getWriter();
+        writer.println("<br>username :"+username);
+        writer.println("<br>password :"+password);
+        writer.println("<br>email"+email);
+        writer.println("<br>birth Date"+birthDate);
+        writer.close();
+    }
+}
